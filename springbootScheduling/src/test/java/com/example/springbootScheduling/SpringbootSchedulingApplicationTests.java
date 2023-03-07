b9eb59ccb33b918c52606d8cd39ada84c04ec28b
Afrah Ali <116621717+Afrahbalushi@users.noreply.github.com>
2023-03-07 11:18:12 +0400

    Scheduling usig Spring boot - reminder

---
+++ springbootScheduling/src/test/java/com/example/springbootScheduling/SpringbootSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootScheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
